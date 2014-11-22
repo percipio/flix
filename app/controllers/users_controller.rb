76b667a06c71130cbe180e5a28e860b7d5bcb811
Joe Hale <jmartyhale@gmail.com>
2014-11-22 13:51:59 -0600

    Update for step-5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			session[:user_id] = user.id
+			session[:user_id] = @user.id
 			redirect_to @user, notice: "Thanks for signing up!"
 		else
 			render :new
@@ -37,6 +37,7 @@ class UsersController < ApplicationController
 	def destroy
 		@user = User.find(params[:id])
 		@user.destroy
+		session[:user_id] = nil
 		redirect_to root_url, alert: "Account successfully deleted!"
 	end
 
