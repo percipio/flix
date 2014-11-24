1187db5a808c3826561f9e79b5d921ee145c8016
Joe Hale <jmartyhale@gmail.com>
2014-11-23 19:22:46 -0600

    Complete step-9-part-1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
+  has_many :critics, through: :reviews, source: :user
 
   validates :title, presence: true
   validates :released_on, :duration, presence: true
